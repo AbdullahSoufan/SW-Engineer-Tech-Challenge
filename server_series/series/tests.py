631e343522862c964109e5c8b10dbb56043770f7
abdallahsoufan <abdallasoufan@kiitos-tech@hotmail.com>
2022-06-24 21:56:17 +0300

    edit_text

--- server_series/series/tests.py
+++ server_series/series/tests.py
@@ -1,2 +1,2 @@
 from django.test import TestCase
-# Unfortunately I ran out of time so i could write any test cases. I wanted to use pytest to validate my code.
+# Unfortunately I ran out of time so i couldn't write any test cases. I wanted to use pytest to validate my code.
